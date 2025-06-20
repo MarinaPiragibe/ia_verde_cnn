d1af92c7d806e9459ccd404c8b584fc956a77d27
Marina Piragibe <marina@coderivy.ai>
2025-06-20 12:38:41 -0300

    feat: explorando o dataset

---
+++ modules/utils.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+
+CLASS_NAMES = ['FAKE', 'REAL']
+
+def plot_sample(train_loader, data_type):
+    plt.figure(figsize=(10, 5))
+    dataiter = iter(train_loader)
+    images, labels = next(dataiter) # Get one batch of training images
+
+    for i in range(min(len(images), 9)): # Plot up to 9 images
+        ax = plt.subplot(1, 9, i + 1)
+        # PyTorch images are (C, H, W), Matplotlib expects (H, W, C)
+        plt.imshow(images[i].numpy().transpose((1, 2, 0)))
+        plt.title(CLASS_NAMES[labels[i]], fontsize=10)
+        plt.axis("off")
+    plt.suptitle(f"CIFAKE {data_type} Images", fontsize=16)
+    plt.tight_layout(rect=[0, 0.03, 1, 1.5]) # Adjust layout to prevent suptitle overlap
+    plt.show()
