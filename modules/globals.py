a45224c8a00aa012c839936a55027fad7876d6bf
Marina Piragibe <marina@coderivy.ai>
2025-06-20 13:11:11 -0300

    feta: update globals + markdown

--- modules/globals.py
+++ modules/globals.py
@@ -1,3 +1,7 @@
 # Altere para o caminho que se encontra o dataset na sua máquina
 DATASET_PATH = r'C:\\Users\\masar\\.cache\\kagglehub\\datasets\\birdy654\\cifake-real-and-ai-generated-synthetic-images\\versions\\3\\'
 CLASS_NAMES = ['FAKE', 'REAL']
+
+IMG_HIGHT = 32
+IMG_WIDTH = 32
+BATCH_SIZE = 32
