2753ad14818d54cb02d05f2d3b0fbd602639d946
Marina Piragibe <marina@coderivy.ai>
2025-06-20 13:29:21 -0300

    feat:__init__

--- modules/utils.py
+++ modules/utils.py
@@ -1,17 +1,16 @@
 from matplotlib import pyplot as plt
-import globals
+from modules import globals
 
 def plot_sample(train_loader, data_type):
     plt.figure(figsize=(10, 5))
     dataiter = iter(train_loader)
-    images, labels = next(dataiter) # Get one batch of training images
+    images, labels = next(dataiter) 
 
-    for i in range(min(len(images), 9)): # Plot up to 9 images
-        ax = plt.subplot(1, 9, i + 1)
-        # PyTorch images are (C, H, W), Matplotlib expects (H, W, C)
+    for i in range(min(len(images), 9)):
+        plt.subplot(1, 9, i + 1)
         plt.imshow(images[i].numpy().transpose((1, 2, 0)))
         plt.title(globals.CLASS_NAMES[labels[i]], fontsize=10)
         plt.axis("off")
     plt.suptitle(f"CIFAKE {data_type} Images", fontsize=16)
-    plt.tight_layout(rect=[0, 0.03, 1, 1.5]) # Adjust layout to prevent suptitle overlap
+    plt.tight_layout(rect=[0, 0.03, 1, 1.5])
     plt.show()
