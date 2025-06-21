4712d63adabfc736354ed22b8c5fb73eaa6539a0
Marina Piragibe <marina@coderivy.ai>
2025-06-21 18:57:32 -0300

    feat: treina o modelo com um subset menor

--- modules/training_functions.py
+++ modules/training_functions.py
@@ -1,88 +1,102 @@
+import torch
 import numpy as np
-from sklearn.metrics import accuracy_score
 import time
-import torch 
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from modules import globals
 
-def train(train_loader, model, epoch, optimizer, cross_entropy_function):
-  model.train()
+def train(train_loader, model, optimizer, cross_entropy_function):
+    model.train()
 
-  start = time.time()
+    start = time.time()
 
-  epoch_loss  = []
-  pred_list = []
-  rotulo_list = []
+    epoch_loss = []
+    pred_list = []
+    rotulo_list = []
 
-  for k, batch in enumerate(train_loader):
-    print(f'{k}/{len(train_loader)}')
-    dado, rotulo = batch
+    for k, batch in enumerate(train_loader):
+        print(f'\r{k+1}/{len(train_loader)}', end='', flush=True) 
 
-    dado = dado.to(globals.DEVICE)
-    rotulo = rotulo.to(globals.DEVICE)
+        dado, rotulo = batch
 
-    # Forward
-    optimizer.zero_grad()
-    ypred = model(dado)
-    loss = cross_entropy_function(ypred, rotulo)
-    epoch_loss.append(loss.cpu().data)
+        dado = dado.to(globals.DEVICE)
+        rotulo = rotulo.to(globals.DEVICE)
 
-    _, pred = torch.max(ypred, axis=1)
+        # Forward
+        optimizer.zero_grad()
+        ypred = model(dado)
+        loss = cross_entropy_function(ypred, rotulo)
+        epoch_loss.append(loss.cpu().data)
 
-    pred_list.extend(pred.cpu().numpy())
-    rotulo_list.extend(rotulo.cpu().numpy())
+        _, pred = torch.max(ypred, axis=1)
 
-    # Backpropagation
-    loss.backward()
-    optimizer.step()
+        pred_list.extend(pred.cpu().numpy())
+        rotulo_list.extend(rotulo.cpu().numpy())
 
-  epoch_loss = np.asarray(epoch_loss)
-  pred_list  = np.asarray(pred_list)
-  rotulo_list  = np.asarray(rotulo_list)
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
 
-  acc = accuracy_score(pred_list, rotulo_list)
+    epoch_loss = np.asarray(epoch_loss)
 
-  end = time.time()
-  print('\nTreino')
-  print(f'Loss: {epoch_loss.mean()} +/- {epoch_loss.std()}, Acc: {acc*100}, Time: {end-start}')
+    final_pred_array = np.asarray(pred_list)
+    final_rotulo_array = np.asarray(rotulo_list)
 
-  return epoch_loss.mean()
+    accuracy = accuracy_score(final_rotulo_array, final_pred_array)
+    precision = precision_score(final_rotulo_array, final_pred_array, average='binary', zero_division=0)
+    recall = recall_score(final_rotulo_array, final_pred_array, average='binary', zero_division=0)
+    f1 = f1_score(final_rotulo_array, final_pred_array, average='binary', zero_division=0)
+    conf_matrix = confusion_matrix(final_rotulo_array, final_pred_array, labels=[0, 1])
 
+    end = time.time()
+    print('\nTreino')
+    print(f'Loss: {epoch_loss.mean():.4f} +/- {epoch_loss.std():.4f}, Acc: {accuracy*100:.2f}, Time: {end-start:.2f}')
 
-def validate(test_loader, model, epoch, cross_entropy_function): 
-  model.eval() 
+    return accuracy, precision, recall, f1, conf_matrix, epoch_loss.mean()
 
-  start = time.time()
+# acc_train, prec_train, rec_train, f1_train, cm_train, loss_train = train(train_loader, resnet_model, epoch, optimizer, criterion)
 
-  epoch_loss  = []
-  pred_list = [] 
-  rotulo_list = [] 
+def validate(test_loader, model, cross_entropy_function):
+    model.eval() 
+    start = time.time()
 
-  with torch.no_grad():
-    for k, batch in enumerate(test_loader):
+    epoch_loss = []
+    pred_list = []
+    rotulo_list = []
 
-      print(f'{k}/{len(test_loader)}')
-      dado, rotulo = batch
+    with torch.no_grad(): # Desativa o cálculo de gradientes
+        for k, batch in enumerate(test_loader):
+            print(f'\r{k+1}/{len(test_loader)}', end='', flush=True) 
 
-      dado = dado.to(globals.DEVICE)
-      rotulo = rotulo.to(globals.DEVICE) 
+            dado, rotulo = batch
 
-      # Forward
-      ypred = model(dado)
-      loss = cross_entropy_function(ypred, rotulo)
-      epoch_loss.append(loss.cpu().data)
+            dado = dado.to(globals.DEVICE)
+            rotulo = rotulo.to(globals.DEVICE)
 
-      _, pred = torch.max(ypred, axis=1)
-      pred_list.extend(pred.cpu().numpy())
-      rotulo_list.extend(rotulo.cpu().numpy())
+            # Forward
+            ypred = model(dado)
+            loss = cross_entropy_function(ypred, rotulo)
+            epoch_loss.append(loss.cpu().data)
 
-  epoch_loss = np.asarray(epoch_loss)
-  pred_list  = np.asarray(pred_list)
-  rotulo_list  = np.asarray(rotulo_list)
+            _, pred = torch.max(ypred, axis=1)
 
-  acc = accuracy_score(pred_list, rotulo_list)
+            pred_list.extend(pred.cpu().numpy())
+            rotulo_list.extend(rotulo.cpu().numpy())
 
-  end = time.time()
-  print('\nValidate')
-  print(f'Loss: {epoch_loss.mean()} +/- {epoch_loss.std()}, Acc: {acc*100}, Time: {end-start}')
+    epoch_loss = np.asarray(epoch_loss)
 
-  return epoch_loss.mean()
+    final_pred_array = np.asarray(pred_list)
+    final_rotulo_array = np.asarray(rotulo_list)
+
+    accuracy = accuracy_score(final_rotulo_array, final_pred_array)
+    precision = precision_score(final_rotulo_array, final_pred_array, average='binary', zero_division=0)
+    recall = recall_score(final_rotulo_array, final_pred_array, average='binary', zero_division=0)
+    f1 = f1_score(final_rotulo_array, final_pred_array, average='binary', zero_division=0)
+    conf_matrix = confusion_matrix(final_rotulo_array, final_pred_array, labels=[0, 1])
+
+    end = time.time()
+    print('\nValidação')
+    print(f'Loss: {epoch_loss.mean():.4f} +/- {epoch_loss.std():.4f}, Acc: {accuracy*100:.2f}, Time: {end-start:.2f}\n')
+
+    return accuracy, precision, recall, f1, conf_matrix, epoch_loss.mean()
+
+# acc_val, prec_val, rec_val, f1_val, cm_val, loss_val = validate(test_loader, resnet_model, epoch, criterion)
