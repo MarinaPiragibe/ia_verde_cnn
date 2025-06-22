0f49ce53ea0f30c36f030b63074d0dd47070563e
Marina Piragibe <marina@coderivy.ai>
2025-06-22 12:27:16 -0300

    feat: quantização

--- modules/globals.py
+++ modules/globals.py
@@ -1,7 +1,7 @@
 # Altere para o caminho que se encontra o dataset na sua máquina
 DATASET_PATH = r'C:\\Users\\masar\\.cache\\kagglehub\\datasets\\birdy654\\cifake-real-and-ai-generated-synthetic-images\\versions\\3\\'
 
-NUM_SAMPLES_TRAIN_DEBUGGER = 100
+NUM_SAMPLES_TRAIN_DEBUGGER = 200
 NUM_SAMPLES_TEST_DEBUGGER = 50
 
 CLASS_NAMES = ['FAKE', 'REAL']
@@ -9,8 +9,8 @@ IMG_HIGHT = 32
 IMG_WIDTH = 32
 
 DEVICE = "cpu"
-EPOCHS_FIRST_PHASE = 2
-EPOCHS_SECOND_PHASE = 3
+EPOCHS_FIRST_PHASE = 1
+EPOCHS_SECOND_PHASE = 2
 LEARNING_RATE_CLASSIFIER = 1e-3
 LEARNING_RATE_FEATURES = 1e-5
 WEIGHT_DECAY = 8e-4
