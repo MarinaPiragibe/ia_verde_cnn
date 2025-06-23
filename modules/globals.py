2df6f39716ca2a37d65ae206ab48804bf6c845dc
Marina Piragibe <marina@coderivy.ai>
2025-06-22 21:13:37 -0300

    prunning

--- modules/globals.py
+++ modules/globals.py
@@ -1,8 +1,8 @@
 # Altere para o caminho que se encontra o dataset na sua máquina
 DATASET_PATH = r'C:\\Users\\masar\\.cache\\kagglehub\\datasets\\birdy654\\cifake-real-and-ai-generated-synthetic-images\\versions\\3\\'
 
-NUM_SAMPLES_TRAIN_DEBUGGER = 200
-NUM_SAMPLES_TEST_DEBUGGER = 50
+NUM_SAMPLES_TRAIN_DEBUGGER = 4000
+NUM_SAMPLES_TEST_DEBUGGER = 1000
 
 CLASS_NAMES = ['FAKE', 'REAL']
 IMG_HIGHT = 32
@@ -10,8 +10,8 @@ IMG_WIDTH = 32
 
 DEVICE = "cpu"
 EPOCHS_FIRST_PHASE = 1
-EPOCHS_SECOND_PHASE = 2
-LEARNING_RATE_CLASSIFIER = 1e-3
-LEARNING_RATE_FEATURES = 1e-5
+EPOCHS_SECOND_PHASE = 1
+LEARNING_RATE_CLASSIFIER = 2e-3
+LEARNING_RATE_FEATURES = 2e-5
 WEIGHT_DECAY = 8e-4
-BATCH_SIZE = 30
+BATCH_SIZE = 16
